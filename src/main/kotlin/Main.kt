a38b49babfba893ec29fc0101df06b5ac9c9e5b2
won983212 <won983212@naver.com>
2022-10-18 20:37:54 +0900

    Initial Commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main() {
+    // 1. 이미 서버가 열려있는지 확인한다.
+    //  1.1. 열려있다면 오류메시지와 함께 종료.
+    // 2. git pull
+    // 3. start minecraft server
+    // 4. git add, git commit, git push
+    println("Hello, world!")
+}
